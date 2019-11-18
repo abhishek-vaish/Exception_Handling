77c23983b18c397dbc7c7898ac9625ceaab03979
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-11-18 17:26:15 +0530

    Main class created. Result method created

--- src/question1/MyFirstException.java
+++ src/question1/MyFirstException.java
@@ -7,3 +7,13 @@ public class MyFirstException extends Exception {
 
 }
 
+class Main {
+    public void result(int num1, int num2) throws MyFirstException {
+        if (num1 + num2 < 150) {
+            throw new MyFirstException("Custom Exception Occurred");
+        } else {
+            System.out.println(num1 + num2);
+        }
+    }
+
+}
